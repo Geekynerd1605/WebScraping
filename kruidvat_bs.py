d8496a1a99b2b7f180920917b99ae59384ea2c53
Geekynerd1605 <133800101+Geekynerd1605@users.noreply.github.com>
2023-06-08 18:15:33 +0530

    Update kruidvat_bs.py

--- kruidvat_bs.py
+++ kruidvat_bs.py
@@ -7,8 +7,6 @@ import requests
 import pandas as pd
 
 options = webdriver.ChromeOptions()
-# options.add_argument('ignore-certificate-errors')
-#options.add_argument("--headless")
 
 driver = webdriver.Chrome(ChromeDriverManager().install(),chrome_options=options)
 
@@ -18,9 +16,6 @@ driver.get("https://www.kruidvat.nl/")
 driver.implicitly_wait(20)
 time.sleep(15)
 driver.find_element("id", "onetrust-accept-btn-handler").click()
-
-print(driver.title)
-print(driver.current_url)
 
 driver.execute_script("window.scrollTo(0, 0);")
 
@@ -35,14 +30,10 @@ time.sleep(1)
 items_list = driver.find_elements("xpath", "//*[@class='nav__link-inner--sub']")
 category_url_list = []
 for items in items_list:
-    print(items.get_attribute('href'))
     category_url_list.append(items.get_attribute('href'))
 
 list_set = set(category_url_list)
 category_url_list = (list(list_set))
-
-print(category_url_list)
-print(len(category_url_list))
 
 product_link_with_roundel = []
 
@@ -61,13 +52,10 @@ for url in category_url_list:
 
         if len(res) > 3:
             number_of_pages = int(res[-2].strip(" "))
-    print(number_of_pages)
-
     base_url = url
 
     for page in range(number_of_pages):
         url = base_url + "?page=" + str(page) + "&size=20&sort=ratingLSP"
-        print(url)
 
         page = requests.get(url)
         soup = BeautifulSoup(page.text, 'html.parser')
@@ -83,10 +71,7 @@ for url in category_url_list:
                 temp = '\t'.join(image['class'])
                 if 'roundel' in temp:
                     if "1003.png" in image['data-src']:
-                        print(link_product)
                         product_link_with_roundel.append(link_product)
-
-print(product_link_with_roundel)
 
 df = pd.DataFrame(product_link_with_roundel)
 
